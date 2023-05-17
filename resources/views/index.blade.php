76dc63716d39f47a4284178a729259ff3278fcbd
semir şeyh sido <100496954+samershiksidi367@users.noreply.github.com>
2023-05-17 13:16:33 +0300

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,18 +6,18 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <link href="https://fonts.googleapis.com/css?family=Muli:300,400,700,900" rel="stylesheet">
-  <link rel="stylesheet" href="{{asset('fonts/icomoon/style.css')}}">
-  <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
-  <link rel="stylesheet" href="{{asset('css/jquery-ui.css')}}">
-  <link rel="stylesheet" href="{{asset('css/owl.carousel.min.css')}}">
-  <link rel="stylesheet" href="{{asset('css/owl.theme.default.min.css')}}">
-  <link rel="stylesheet" href="{{asset('css/owl.theme.default.min.css')}} ">
-  <link rel="stylesheet" href="{{asset('css/jquery.fancybox.min.css')}} ">
-  <link rel="stylesheet" href="{{asset('css/css/bootstrap-datepicker.css')}} ">
-  <link rel="stylesheet" href="{{asset('css/fonts/flaticon/font/flaticon.css')}} ">
-  <link rel="stylesheet" href="{{asset('css/css/aos.css')}}">
-  <link href=" {{asset('css/css/jquery.mb.YTPlayer.min.css')}} " media="all" rel="stylesheet" type="text/css">
-  <link rel="stylesheet" href="{{asset('css/css/style.css')}} ">
+  <link rel="stylesheet" href="{{asset('/fonts/icomoon/style.css')}}">
+  <link rel="stylesheet" href="{{asset('/css/bootstrap.min.css')}}">
+  <link rel="stylesheet" href="{{asset('/css/jquery-ui.css')}}">
+  <link rel="stylesheet" href="{{asset('/css/owl.carousel.min.css')}}">
+  <link rel="stylesheet" href="{{asset('/css/owl.theme.default.min.css')}}">
+  <link rel="stylesheet" href="{{asset('/css/owl.theme.default.min.css')}} ">
+  <link rel="stylesheet" href="{{asset('/css/jquery.fancybox.min.css')}} ">
+  <link rel="stylesheet" href="{{asset('/css/bootstrap-datepicker.css')}} ">
+  <link rel="stylesheet" href="{{asset('/css/fonts/flaticon/font/flaticon.css')}} ">
+  <link rel="stylesheet" href="{{asset('/css/aos.css')}}">
+  <link href=" {{asset('/css/jquery.mb.YTPlayer.min.css')}} " media="all" rel="stylesheet" type="text/css">
+  <link rel="stylesheet" href="{{asset('/css/style.css')}} ">
 
 
 
@@ -636,21 +636,21 @@
   <!-- loader -->
   <div id="loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#51be78"/></svg></div>
 
-  <script src="{{asset('js/jquery-3.3.1.min.js')}} "></script>
-  <script src="{{asset('js/jquery-migrate-3.0.1.min.js')}}"></script>
-  <script src="{{asset('js/js/jquery-ui.js')}} "></script>
-  <script src="{{asset('js/js/popper.min.js')}} "></script>
-  <script src="{{asset('js/js/bootstrap.min.js')}} "></script>
-  <script src="{{asset('js/js/owl.carousel.min.js')}} "></script>
-  <script src="{{asset('js/js/jquery.stellar.min.js')}} "></script>
-  <script src="{{asset('js/jquery.countdown.min.js')}} "></script>
-  <script src="{{asset('js/bootstrap-datepicker.min.js')}} "></script>
-  <script src="{{asset('js/jquery.easing.1.3.js')}} "></script>
-  <script src="{{asset('js/aos.js')}} "></script>
-  <script src="{{asset('js/jquery.fancybox.min.js')}} "></script>
-  <script src="{{asset('js/jquery.sticky.js')}} "></script>
-  <script src="{{asset('js/jquery.mb.YTPlayer.min.js')}} "></script>
-  <script src="{{asset('js/main.js')}} "></script>
+  <script src="{{asset('/js/jquery-3.3.1.min.js')}} "></script>
+  <script src="{{asset('/js/jquery-migrate-3.0.1.min.js')}}"></script>
+  <script src="{{asset('/js/jquery-ui.js')}} "></script>
+  <script src="{{asset('/js/popper.min.js')}} "></script>
+  <script src="{{asset('/js/bootstrap.min.js')}} "></script>
+  <script src="{{asset('/js/owl.carousel.min.js')}} "></script>
+  <script src="{{asset('/js/jquery.stellar.min.js')}} "></script>
+  <script src="{{asset('/js/jquery.countdown.min.js')}} "></script>
+  <script src="{{asset('/js/bootstrap-datepicker.min.js')}} "></script>
+  <script src="{{asset('/js/jquery.easing.1.3.js')}} "></script>
+  <script src="{{asset('/js/aos.js')}} "></script>
+  <script src="{{asset('/js/jquery.fancybox.min.js')}} "></script>
+  <script src="{{asset('/js/jquery.sticky.js')}} "></script>
+  <script src="{{asset('/js/jquery.mb.YTPlayer.min.js')}} "></script>
+  <script src="{{asset('/js/main.js')}} "></script>
 
 </body>
 
